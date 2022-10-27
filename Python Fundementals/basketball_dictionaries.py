c759f5e7dc58f41abf941473ca18666cfd552a8a
BHagge26 <bhagge92@gmail.com>
2022-10-27 08:55:40 -0700

    wk1day4algo

---
+++ Python Fundementals/basketball_dictionaries.py
@@ -0,0 +1,114 @@
+#When you only want to pass in one argument for all attributes, pass data as the argument and change the values on the right side to 
+# = data['_____']
+
+class Player:
+    def __init__(self, data):
+        self.name = data['name']
+        self.age = data['age']
+        self.position = data['position'] 
+        self.team = data['team']
+
+
+kevin = {
+    "name": "Kevin Durant", 
+    "age":34, 
+    "position": "small forward", 
+    "team": "Brooklyn Nets"
+}
+jason = {
+    "name": "Jason Tatum", 
+    "age":24, 
+    "position": "small forward", 
+    "team": "Boston Celtics"
+}
+kyrie = {
+    "name": "Kyrie Irving", 
+    "age":32,
+    "position": "Point Guard", 
+    "team": "Brooklyn Nets"
+}
+
+player_kevin = Player(kevin)
+print(player_kevin)
+
+player_jason = Player(jason)
+print(player_jason)
+
+player_kyrie = Player(kyrie)
+print(player_kyrie)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+players = [
+    {
+        "name": "Kevin Durant", 
+        "age":34, 
+        "position": "small forward", 
+        "team": "Brooklyn Nets"
+    },
+    {
+        "name": "Jason Tatum", 
+        "age":24, 
+        "position": "small forward", 
+        "team": "Boston Celtics"
+    },
+    {
+        "name": "Kyrie Irving", 
+        "age":32,
+        "position": "Point Guard", 
+        "team": "Brooklyn Nets"
+    },
+    {
+        "name": "Damian Lillard", 
+        "age":33,
+        "position": "Point Guard", 
+        "team": "Portland Trailblazers"
+    },
+    {
+        "name": "Joel Embiid", 
+        "age":32,
+        "position": "Power Foward", 
+        "team": "Philidelphia 76ers"
+    },
+    {
+        "name": "DeMar DeRozan",
+        "age": 32,
+        "position": "Shooting Guard",
+        "team": "Chicago Bulls"
+    }
+]
+
+
+
+New_Player_Team = []
+for dictionary in list:
+    player = Player(dictionary)
+    New_Player_Team.append(player)
+
+print(New_Player_Team)
+
+
+# @classmethod --------------------------------------Ask Spencer to explain this tomorrow
+#     def add_players(cls, data):
+#         player_objects = []
+#         for dict in data:
+#             player_objects.append(cls(dict))
+#         return player_objects
